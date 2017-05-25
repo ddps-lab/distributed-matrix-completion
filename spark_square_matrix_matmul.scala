98b6ab07d04188532e3bd0701696254f5279c3e9
NamHyunE <something0094@gmail.com>
2017-05-25 13:28:08 +0900

    test commit

--- spark_square_matrix_matmul.scala
+++ spark_square_matrix_matmul.scala
@@ -1,27 +1,36 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 import org.apache.spark.mllib.linalg.Matrices
 import org.apache.spark.mllib.linalg.distributed.BlockMatrix
 
-val rowParallelism = 4 
-val colParallelism = 4
-val rowBlockSize = 2048
-val colBlockSize = 2048
+object Main {
+    def main(args: Array[String]) {
+        val conf = new SparkConf().setMaster("local").setAppName("My App")
+        val sc = new SparkContext(conf)
 
-val rows = rowParallelism * rowBlockSize
-val cols = colParallelism * colBlockSize
+        val rowParallelism = 4
+        val colParallelism = 4
+        val rowBlockSize = 2048
+        val colBlockSize = 2048
 
-val rowBlocks = rows/rowBlockSize
-val colBlocks = cols/colBlockSize
+        val rows = rowParallelism * rowBlockSize
+        val cols = colParallelism * colBlockSize
 
-val left_rdd = sc.parallelize( {for(i <- 0 until rowBlocks; j <- 0 until colBlocks) yield (i, j)}, rowParallelism*colParallelism).map( coord => (coord, Matrices.rand(rowBlockSize, colBlockSize, util.Random.self)))
+        val rowBlocks = rows/rowBlockSize
+        val colBlocks = cols/colBlockSize
 
-val right_rdd = sc.parallelize( {for(i <- 0 until rowBlocks; j <- 0 until colBlocks) yield (i, j)}, rowParallelism*colParallelism).map( coord => (coord, Matrices.rand(rowBlockSize, colBlockSize, util.Random.self)))
+        val left_rdd = sc.parallelize( {for(i <- 0 until rowBlocks; j <- 0 until colBlocks) yield (i, j)}, rowParallelism*colParallelism).map( coord => (coord, Matrices.rand(rowBlockSize, colBlockSize, util.Random.self)))
 
-val left_bm = new BlockMatrix(left_rdd, rowBlockSize, colBlockSize).cache()
-val right_bm = new BlockMatrix(right_rdd, rowBlockSize, colBlockSize).cache()
+        val right_rdd = sc.parallelize( {for(i <- 0 until rowBlocks; j <- 0 until colBlocks) yield (i, j)}, rowParallelism*colParallelism).map( coord => (coord, Matrices.rand(rowBlockSize, colBlockSize, util.Random.self)))
 
-val t = System.nanoTime()
-val result = left_bm.multiply(right_bm)
-result.validate()
-val latency = (System.nanoTime() - t) / 1e9
-println(latency)
+        val left_bm = new BlockMatrix(left_rdd, rowBlockSize, colBlockSize).cache()
+        val right_bm = new BlockMatrix(right_rdd, rowBlockSize, colBlockSize).cache()
 
+        val t = System.nanoTime()
+        val result = left_bm.multiply(right_b,4)
+        result.validate()
+        val latency = (System.nanoTime() - t) / 1e9
+        println(latency)
+        println("finish!!!")
+    }
+}
